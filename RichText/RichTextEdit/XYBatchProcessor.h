b3bab760f9a0e4e0f6ece96abdff478680f45b87
xieyan <1515489649@qq.com>
2017-06-23 07:49:06 +0800

    complete processer
    complete processer

--- RichText/RichTextEdit/XYBatchProcessor.h
+++ RichText/RichTextEdit/XYBatchProcessor.h
@@ -13,7 +13,29 @@ extern NSString* const keyNotificationXYBatchProcesserReturn;
 @end
 @interface XYBatchProcessor : NSObject
 +(instancetype)instance;
--(NSString*)batchProcessObjects:(NSArray*)objects convertor:(id(^)(id obj))convertor;
+-(NSString*)batchProcessObjects:(NSArray*)objects;
+
+-(void)suspend:(NSString*)identifier;
+-(void)cancel:(NSString*)identifier;
+
+
+-(void)convertObject:(id)obj complete:(void(^)(id obj))complete;
+
+-(void)saveObject:(id)object complete:(void(^)(NSString* retriveid))complete;
+-(void)retriveObjectforId:(NSString*)retriveid complete:(void(^)(id retriveObj))complete;
+
+-(id<XYBatchProcessorCancelable>)processObject:(id)object complete:(void(^)(id msg))complete;
+
+-(void)processCompleteId:(NSString*)identifier result:(NSArray*)result;
+@end
+
+@class XYRichTextImage;
+@interface XYImageUploader : XYBatchProcessor
+-(NSString*)batchProcessObjects:(NSArray<XYRichTextImage*>*)objects;
+
+
+
+-(void)convertObject:(XYRichTextImage*)obj complete:(void(^)(NSData* obj))complete;
 -(void)saveObject:(id)object complete:(void(^)(NSString* retriveid))complete;
 -(void)retriveObjectforId:(NSString*)retriveid complete:(void(^)(id retriveObj))complete;
 -(id<XYBatchProcessorCancelable>)processObject:(id)object complete:(void(^)(id msg))complete;
--- RichText/RichTextEdit/XYBatchProcessor.m
+++ RichText/RichTextEdit/XYBatchProcessor.m
@@ -8,9 +8,39 @@
 
 #import "XYBatchProcessor.h"
 NSString* const keyNotificationXYBatchProcessorReturn = @"keyNotificationXYBatchProcessorReturn";
+typedef NS_ENUM(NSUInteger,_XYProcessStatus) {
+    _XYProcessStatusRunning,
+    _XYProcessStatusCanceled,
+    _XYProcessStatusSuspended,
+    _XYProcessStatusDone,
+};
+@interface _XYBatchProcessorModel : NSObject
+@property(nonatomic,assign)_XYProcessStatus status;
+
+@property(nonatomic,strong)NSMutableArray* batchResults;
+@property(nonatomic,strong)NSMutableArray* batchCaches;
+@property(nonatomic,strong)NSString* batchIdentifier;
+
+@property(nonatomic,assign)NSInteger index;
+@property(nonatomic,strong)id msg;
++(instancetype)model:(NSMutableArray*)results caches:(NSMutableArray*)caches ident:(NSString*)ident msg:(id)msg  index:(NSInteger)index;
+@end
+@implementation _XYBatchProcessorModel
++(instancetype)model:(NSMutableArray*)results caches:(NSMutableArray*)caches ident:(NSString*)ident msg:(id)msg  index:(NSInteger)index{
+    _XYBatchProcessorModel* model = [_XYBatchProcessorModel new];
+    model.batchResults = results;
+    model.batchCaches = caches;
+    model.batchIdentifier = ident;
+    model.msg = msg;
+    model.index = index;
+    model.status = _XYProcessStatusRunning;
+    return model;
+}
+@end
 @interface XYBatchProcessor ()
-@property(nonatomic,strong)NSMutableDictionary* processDic;
+@property(nonatomic,strong)NSMutableDictionary<NSString*,NSMutableArray<_XYBatchProcessorModel*>*>* processDic;
 @property(nonatomic,strong)NSOperationQueue* processQueue;
+@property (nonatomic, assign) NSUInteger objCount;
 @end
 @implementation XYBatchProcessor
 static XYBatchProcessor* processor = nil;
@@ -28,39 +58,90 @@ static XYBatchProcessor* processor = nil;
     if (self) {
         self.processDic = [NSMutableDictionary dictionary];
         self.processQueue = [NSOperationQueue new];
+        self.objCount = 0;
     }
     return self;
 }
 
 -(NSString*)getUniqueId{
-    return @"";
+    self.objCount ++;
+    return [NSString stringWithFormat:@"XYBatchProcess_%ld",(unsigned long)self.objCount];
 }
--(NSString*)batchProcessObjects:(NSArray*)objects convertor:(id(^)(id obj))convertor{
+-(NSString*)batchProcessObjects:(NSArray*)objects{
     NSString* identifier = [self getUniqueId];
     NSMutableArray *infoArr = [NSMutableArray array];
     for (NSInteger i = 0; i < objects.count; i++) { [infoArr addObject:@1]; }
-    self.processDic[identifier] = objects;
+    NSMutableArray *cacheArray = infoArr.mutableCopy;
+    NSMutableArray *modelArray = [NSMutableArray arrayWithCapacity:objects.count];
     for (NSInteger i = 0; i < objects.count; i++) {
-        [self.processQueue addOperationWithBlock:^{
-            [self processObject:convertor(objects[i]) complete:^(id msg) {
-                [infoArr replaceObjectAtIndex:i withObject:msg];
-                for (id item in infoArr) { if ([item isKindOfClass:[NSNumber class]]) return; }
-                [self processCompleteId:identifier result:infoArr];
-            }];
-        }];
+        _XYBatchProcessorModel* model = [_XYBatchProcessorModel model:infoArr caches:cacheArray ident:identifier msg:objects[i] index:i];
+        NSInvocationOperation * oper = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(_ConvertModel:) object:model];
+        [self.processQueue addOperation:oper];
+        [modelArray addObject:model];
     }
+    self.processDic[identifier] = modelArray;
     return identifier;
+}
+
+-(void)suspend:(NSString*)identifier{
+    [self.processQueue setSuspended:YES];
+    
+}
+-(void)cancel:(NSString*)identifier{
+    
+}
+-(void)saveSatus:(NSString*)identifier{
+    
+}
+
+-(void)_ConvertModel:(_XYBatchProcessorModel*)model{
+    [self convertObject:model.msg complete:^(id obj) {
+        model.msg = obj;
+        NSInvocationOperation * oper = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(_ProcessModel:) object:model];
+        [self.processQueue addOperation:oper];
+        NSInvocationOperation * oper1 = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(_SaveModel:) object:model];
+        [self.processQueue addOperation:oper1];
+    }];
+}
+-(void)convertObject:(id)obj complete:(void (^)(id))complete{
+    
+}
+
+-(void)_SaveModel:(_XYBatchProcessorModel*)model{
+    [self saveObject:model.msg complete:^(NSString *retriveid) {
+        [model.batchCaches replaceObjectAtIndex:model.index withObject:retriveid];
+        for (id item in model.batchCaches) { if ([item isKindOfClass:[NSNumber class]]) return; }
+        
+    }];
 }
 -(void)saveObject:(id)object complete:(void(^)(NSString* retriveid))complete{
     
 }
+
+
 -(void)retriveObjectforId:(NSString*)retriveid complete:(void(^)(id retriveObj))complete{
     
+}
+
+
+-(void)_ProcessModel:(_XYBatchProcessorModel*)model {
+    [self processObject:model.msg complete:^(id msg) {
+        [model.batchResults replaceObjectAtIndex:model.index withObject:msg];
+        model.status = _XYProcessStatusDone;
+        for (id item in model.batchResults) { if ([item isKindOfClass:[NSNumber class]]) return; }
+        NSInvocationOperation * oper = [[NSInvocationOperation alloc] initWithTarget:self selector:@selector(_ProcessComplete:) object:model];
+        [self.processQueue addOperation:oper];
+    }];
 }
 -(id<XYBatchProcessorCancelable>)processObject:(id)object complete:(void(^)(id msg))complete{
     return nil;
 }
 
+
+-(void)_ProcessComplete:(_XYBatchProcessorModel*)model{
+    [self processCompleteId:model.batchIdentifier result:model.batchResults];
+    
+}
 -(void)processCompleteId:(NSString*)identifier result:(NSArray*)result {
     
 }
