c9b532182cf97d1080edc77ace79853af3b3948f
xyzhenu <1515489649@qq.com>
2017-06-22 15:08:51 +0800

    complete animation and layout
    complete animation and layout

--- RichText/RichTextEdit/XYRichTextVC.h
+++ RichText/RichTextEdit/XYRichTextVC.h
@@ -9,9 +9,8 @@
 #import <Photos/Photos.h>
 NS_ASSUME_NONNULL_BEGIN
 typedef NS_ENUM(NSUInteger,XYRichTextImageSaveType) {
-    XYRichTextImageSaveTypeNone = 1,
     XYRichTextImageSaveTypeOnInsert = 1<<1,
-    XYRichTextImageSaveTypeOnSaveClick = 1<<2,
+    XYRichTextImageSaveTypeOnSaveClick = 1<<2,//default
 };
 @interface XYRichTextImage : NSObject
 @property (nonatomic,strong,nullable)PHAsset* asset;
@@ -19,11 +18,12 @@ typedef NS_ENUM(NSUInteger,XYRichTextImageSaveType) {
 @property (nonatomic,strong,nullable)NSDictionary* info;
 @property (nonatomic,strong,nullable)NSURL* url;
 @property (nonatomic,strong,nullable)NSString* identifier;
+@property (nonatomic,assign)BOOL isSelectOriginalPhoto;//default no
 @end
 @interface XYRichTextVC : UIViewController
 @property (nonatomic,strong)NSString* picSymbolPrefix;//default [
 @property (nonatomic,strong)NSString* picSymbolSuffix;//default ]
-- (void)pickImage:(void(^)(NSArray<PHAsset*>* assets, NSArray<UIImage*>*images,NSArray<NSDictionary *> *infos))complete;
+- (void)pickImage:(void(^)(NSArray<PHAsset*>* assets, NSArray<UIImage*>*images,NSArray<NSDictionary *> *infos,BOOL isSelectOriginalPhoto))complete;
 @property(nonatomic,assign)XYRichTextImageSaveType saveType;
 - (void)onSaveWithText:(NSString*)content images:(NSDictionary<NSString*,XYRichTextImage*>*)images complete:(BOOL)complete;
 @end
--- RichText/RichTextEdit/XYRichTextVC.m
+++ RichText/RichTextEdit/XYRichTextVC.m
@@ -18,38 +18,51 @@ static NSString* const keyRichTextImage = @"keyRichTextImage";
 @end
 
 @implementation XYRichTextImage
-
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.isSelectOriginalPhoto = NO;
+    }
+    return self;
+}
 @end
 @interface XYRichTextVC ()<YYTextViewDelegate, YYTextKeyboardObserver>
 @property (nonatomic, assign) YYTextView *textView;
 @property (nonatomic, assign) NSUInteger imageCount;
+@property (nonatomic, strong) NSLayoutConstraint* toolBarBottom;
 @end
 
 @implementation XYRichTextVC
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.picSymbolPrefix = @"[";
     self.picSymbolSuffix = @"]";
     self.imageCount = 0;
-    self.saveType = XYRichTextImageSaveTypeNone;
+    self.saveType = XYRichTextImageSaveTypeOnSaveClick;
     self.automaticallyAdjustsScrollViewInsets = NO;
     
     UIBarButtonItem *buttonItemSave = [[UIBarButtonItem alloc] initWithTitle:@"存草稿" style:UIBarButtonItemStylePlain target:self action:@selector(saveClick:)];
-    UIBarButtonItem *buttonItemDone = [[UIBarButtonItem alloc] initWithTitle:@"完成" style:UIBarButtonItemStylePlain target:self action:@selector(saveClick:)];
+    UIBarButtonItem *buttonItemDone = [[UIBarButtonItem alloc] initWithTitle:@"完成" style:UIBarButtonItemStylePlain target:self action:@selector(doneClick:)];
     self.navigationItem.rightBarButtonItems = @[buttonItemSave,buttonItemDone];
     
     
     UIView *toolbar = [[UIVisualEffectView alloc] initWithEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleExtraLight]];
-    toolbar.size = CGSizeMake(kScreenWidth, 40);
-    toolbar.top = kiOS7Later ? 64 : 0;
     [self.view addSubview:toolbar];
-    
+    toolbar.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[toolbar]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(toolbar)]];
+    [toolbar addConstraint:[NSLayoutConstraint constraintWithItem:toolbar attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:40]];
+    self.toolBarBottom = [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:toolbar attribute:NSLayoutAttributeBottom multiplier:1 constant:0];
+    [self.view addConstraint:self.toolBarBottom];
 
     UIButton* alubmBtn = [UIButton buttonWithType:UIButtonTypeContactAdd];
     alubmBtn.size = CGSizeMake(44, 40);
     [alubmBtn addTarget:self action:@selector(addImageClick) forControlEvents:UIControlEventTouchUpInside];
     [toolbar addSubview:alubmBtn];
-    alubmBtn.right = toolbar.width;
+    alubmBtn.translatesAutoresizingMaskIntoConstraints = NO;
+    [toolbar addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[alubmBtn(40)]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(alubmBtn)]];
+    [toolbar addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[alubmBtn]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(alubmBtn)]];
     
     NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithString:@"It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the season of light, it was the season of darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us. We were all going direct to heaven, we were all going direct the other way.\n\n这是最好的时代，这是最坏的时代；这是智慧的时代，这是愚蠢的时代；这是信仰的时期，这是怀疑的时期；这是光明的季节，这是黑暗的季节；这是希望之春，这是失望之冬；人们面前有着各样事物，人们面前一无所有；人们正在直登天堂，人们正在直下地狱。"];
     text.font = [UIFont systemFontOfSize:20];
@@ -73,16 +86,20 @@ static NSString* const keyRichTextImage = @"keyRichTextImage";
     textView.selectedRange = NSMakeRange(text.length, 0);
     [self.view insertSubview:textView belowSubview:toolbar];
     self.textView = textView;
+    textView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[textView]-0-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(textView)]];
+    id toplayout = self.topLayoutGuide;
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[toplayout]-0-[textView]-0-[toolbar]" options:0 metrics:nil views:NSDictionaryOfVariableBindings(textView,toolbar,toplayout)]];
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.6 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [textView becomeFirstResponder];
     });
     
-    [[YYTextKeyboardManager defaultManager] addObserver:self];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(UIKeyboardWillChangeFrameNotification:) name:UIKeyboardWillChangeFrameNotification object:nil];
 }
 
 - (void)dealloc {
-    [[YYTextKeyboardManager defaultManager] removeObserver:self];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)saveClick:(UIBarButtonItem *)item {
@@ -97,50 +114,50 @@ static NSString* const keyRichTextImage = @"keyRichTextImage";
     [alert addAction:[UIAlertAction actionWithTitle:@"发布" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         [self saveComplete:YES];
     }]];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 #pragma mark - keyboard
 
-- (void)keyboardChangedWithTransition:(YYTextKeyboardTransition)transition {
-    BOOL clipped = NO;
-    if (self.textView.isVerticalForm && transition.toVisible) {
-        CGRect rect = [[YYTextKeyboardManager defaultManager] convertRect:transition.toFrame toView:self.view];
-        if (CGRectGetMaxY(rect) == self.view.height) {
-            CGRect textFrame = self.view.bounds;
-            textFrame.size.height -= rect.size.height;
-            self.textView.frame = textFrame;
-            clipped = YES;
+- (void)UIKeyboardWillChangeFrameNotification:(NSNotification*)transition {
+    CGRect frame = [transition.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    NSTimeInterval time = [transition.userInfo[UIKeyboardAnimationDurationUserInfoKey] floatValue];
+    [UIView animateWithDuration:time animations:^{
+        self.textView.frame = CGRectMake(self.textView.origin.x, self.textView.origin.y, self.textView.size.width, self.view.height-self.textView.origin.y-frame.size.height);
+    } completion:^(BOOL finished) {
+        if (finished) {
+            self.toolBarBottom.constant = frame.origin.y >= self.view.frame.size.height ? 0 : frame.size.height;
         }
-    }
-    
-    if (!clipped) {
-        self.textView.frame = self.view.bounds;
-    }
+    }];
 }
 
 #pragma mark - pick image
 
 - (void)addImageClick {
-    [self pickImage:^(NSArray<PHAsset *> * _Nonnull assets, NSArray<UIImage *> * _Nonnull images,NSArray<NSDictionary *> *infos) {
+    [self pickImage:^(NSArray<PHAsset *> * _Nonnull assets, NSArray<UIImage *> * _Nonnull images,NSArray<NSDictionary *> *infos,BOOL isSelectOriginalPhoto) {
         for (int i = 0; i < assets.count; i++) {
             XYRichTextImage* image = [XYRichTextImage new];
             image.asset = assets[i];
             image.image = images[i];
             image.info = infos[i];
             image.url = image.info[@"PHImageFileURLKey"];
+            image.isSelectOriginalPhoto = isSelectOriginalPhoto;
             [self.textView insertImage:image];
+        }
+        if (self.saveType & XYRichTextImageSaveTypeOnInsert) {
+            [self saveComplete:NO];
         }
     }];
 }
-- (void)pickImage:(void(^)(NSArray<PHAsset*>* assets, NSArray<UIImage*>*images,NSArray<NSDictionary *> *infos))complete {
+- (void)pickImage:(void(^)(NSArray<PHAsset*>* assets, NSArray<UIImage*>*images,NSArray<NSDictionary *> *infos,BOOL isSelectOriginalPhoto))complete {
     TZImagePickerController *imagePickerVc = [[TZImagePickerController alloc] initWithMaxImagesCount:3 delegate:nil];
     imagePickerVc.photoWidth = (self.textView.width - self.textView.textContainerInset.left - self.textView.textContainerInset.right);
     imagePickerVc.allowPickingGif = YES;
     [imagePickerVc setDidFinishPickingPhotosWithInfosHandle:^(NSArray<UIImage *> *photos,NSArray *assets,BOOL isSelectOriginalPhoto,NSArray<NSDictionary *> *infos){
-        complete(assets,photos,infos);
+        complete(assets,photos,infos,isSelectOriginalPhoto);
     }];
     [imagePickerVc setDidFinishPickingGifImageHandle:^(UIImage *animatedImage,id sourceAssets){
-        complete(@[sourceAssets],@[animatedImage],@[@{}]);
+        complete(@[sourceAssets],@[animatedImage],@[@{}],YES);
     }];
     [self presentViewController:imagePickerVc animated:YES completion:nil];
 }
@@ -148,7 +165,7 @@ static NSString* const keyRichTextImage = @"keyRichTextImage";
 #pragma mark - save image
 - (NSString*)generateId{
     self.imageCount ++;
-    return [NSString stringWithFormat:@"%ld",(unsigned long)self.imageCount];
+    return [NSString stringWithFormat:@"XYRichText_%ld",(unsigned long)self.imageCount];
 }
 
 - (NSArray *)rangesOfString:(NSString *)searchString inString:(NSString *)str {
